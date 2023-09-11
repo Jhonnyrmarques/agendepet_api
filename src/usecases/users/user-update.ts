06f841518131f5d6f3f1c55dd55361e0971c5672
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-11 19:15:21 -0300

    feat: create user update use case

---
+++ src/usecases/users/user-update.ts
@@ -0,0 +1,42 @@
+import { UserNotExistError } from '@/errors/user-not-exist-error'
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+
+interface UserUpdateUseCaseRequest {
+  id: string
+  first_name?: string
+  last_name?: string
+  phone?: string
+}
+
+interface UserUpdateUseCaseResponse {
+  user: User
+}
+
+export class UserUpdateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    id,
+    first_name,
+    last_name,
+    phone,
+  }: UserUpdateUseCaseRequest): Promise<UserUpdateUseCaseResponse> {
+    const userExists = await this.usersRepository.findUserById(id)
+
+    if (!userExists) {
+      throw new UserNotExistError()
+    }
+
+    const user = await this.usersRepository.userUpdate(
+      id,
+      first_name,
+      last_name,
+      phone,
+    )
+
+    return {
+      user,
+    }
+  }
+}

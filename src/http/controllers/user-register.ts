03c085f096b12e9f48bde35c46e981927921402e
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-11 17:40:56 -0300

    refactor: changed to the use case receiving the factory function

--- src/http/controllers/user-register.ts
+++ src/http/controllers/user-register.ts
@@ -2,8 +2,7 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { UserRegisterUseCase } from '@/usecases/users/user-register'
+import { makeUserRegisterUseCase } from '@/usecases/factories/make-user-register-use-case'
 
 export async function userRegister(
   request: FastifyRequest,
@@ -20,8 +19,7 @@ export async function userRegister(
   const { first_name, last_name, email, password_hash, phone } =
     userRegisterBodySchema.parse(request.body)
 
-  const usersRepository = new PrismaUsersRepository()
-  const userRegisterUseCase = new UserRegisterUseCase(usersRepository)
+  const userRegisterUseCase = makeUserRegisterUseCase()
 
   try {
     await userRegisterUseCase.execute({

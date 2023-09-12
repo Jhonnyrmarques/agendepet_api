63d748243704c17228445143214f3add8dd084f6
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-11 23:21:58 -0300

    feat: create userUpdate controller

---
+++ src/http/controllers/user-update.ts
@@ -0,0 +1,41 @@
+import { UserNotExistError } from '@/errors/user-not-exist-error'
+import { makeUserUpdateUseCase } from '@/usecases/factories/make-user-update-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function userUpdate(request: FastifyRequest, reply: FastifyReply) {
+  const userUpdateBodySchema = z.object({
+    first_name: z.string().optional(),
+    last_name: z.string().optional(),
+    phone: z.string().optional(),
+  })
+
+  const { first_name, last_name, phone } = userUpdateBodySchema.parse(
+    request.body,
+  )
+
+  const userUpdateUseCase = makeUserUpdateUseCase()
+
+  try {
+    const { user } = await userUpdateUseCase.execute({
+      id: request.user.sub,
+      first_name,
+      last_name,
+      phone,
+    })
+
+    return reply.status(200).send({
+      user: {
+        ...user,
+        password_hash: undefined,
+        isAdmin: undefined,
+      },
+    })
+  } catch (err) {
+    if (err instanceof UserNotExistError) {
+      reply.status(404).send({ message: err.message })
+    }
+
+    throw err
+  }
+}

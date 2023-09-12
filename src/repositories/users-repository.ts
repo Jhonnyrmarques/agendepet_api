5899f0ef90ef13a5a8fbf18c7ec31f7bb49e494a
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-11 23:24:09 -0300

    refactor: passed the Prisma.UserUpdateInput parameter to the update method

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -4,10 +4,5 @@ export interface UsersRepository {
   create(data: Prisma.UserCreateInput): Promise<User>
   findUserByEmail(email: string): Promise<User | null>
   findUserById(id: string): Promise<User | null>
-  userUpdate(
-    id: string,
-    first_name?: string,
-    last_name?: string,
-    phone?: string,
-  ): Promise<User>
+  userUpdate(data: Prisma.UserUpdateInput): Promise<User>
 }

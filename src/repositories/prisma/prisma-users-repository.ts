81c37807141266d1990bfcb81e304f721ec78b6c
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-11 21:13:54 -0300

    feat: implemented findUserById and userUpdate methods

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -20,4 +20,34 @@ export class PrismaUsersRepository implements UsersRepository {
 
     return user
   }
+
+  async findUserById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
+  async userUpdate(
+    id: string,
+    first_name?: string,
+    last_name?: string,
+    phone?: string,
+  ) {
+    const user = await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        first_name,
+        last_name,
+        phone,
+      },
+    })
+
+    return user
+  }
 }

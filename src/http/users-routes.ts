e86a9f266a51915fba5834adebf6c9e3349aada0
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-11 23:21:06 -0300

    feat: add in usersRoutes routes of sessions, profile and update

--- src/http/users-routes.ts
+++ src/http/users-routes.ts
@@ -1,6 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { userRegister } from './controllers/user-register'
+import { authentication } from './controllers/authentication'
+import { userProfile } from './controllers/user-profile'
+import { verifyJWT } from '@/middlewares/verify-jwt'
+import { userUpdate } from './controllers/user-update'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', userRegister)
+  app.post('/sessions', authentication)
+
+  app.get('/profile', { onRequest: [verifyJWT] }, userProfile)
+  app.put('/users', { onRequest: [verifyJWT] }, userUpdate)
 }

ea1b3edf2ad0e56a2ab546dcf0737035c864772d
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-22 17:39:52 -0300

    chore: add JWT_TOKEN_EXPIRESS and JWT_REFRESH_TOKEN_EXPIRESS validation

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.string(),
   JWT_SECRET: z.string(),
+  JWT_TOKEN_EXPIRESS: z.string(),
+  JWT_REFRESH_TOKEN_EXPIRESS: z.string(),
   PORT: z.coerce.number(),
 })
 

fb9af7bfad684a3ed52b22c73da88f0c0db3ebe1
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-22 17:45:46 -0300

    feat: add refresh route

--- src/http/controllers/users/users-routes.ts
+++ src/http/controllers/users/users-routes.ts
@@ -5,10 +5,13 @@ import { userProfile } from './user-profile'
 
 import { verifyJWT } from '@/middlewares/verify-jwt'
 import { userUpdate } from './user-update'
+import { refreshToken } from './refresh-token'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', userRegister)
   app.post('/sessions', authentication)
+
+  app.patch('/token/refresh', refreshToken)
 
   app.get('/profile', { onRequest: [verifyJWT] }, userProfile)
   app.put('/users', { onRequest: [verifyJWT] }, userUpdate)

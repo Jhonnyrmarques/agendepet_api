5b5f3448a1e91df9c10fb643519e8fbd09cfc2fd
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-22 19:47:40 -0300

    feat: add fetchBreeds method

--- src/repositories/breeds-repository.ts
+++ src/repositories/breeds-repository.ts
@@ -5,4 +5,5 @@ export interface BreedsRepository {
   findBreedByName(name: string): Promise<Breed | null>
   update(data: Prisma.BreedUpdateInput): Promise<void>
   findBreedById(id: string): Promise<Breed | null>
+  fetchBreeds(query: string, page: number): Promise<Breed[]>
 }

f0ed5c9d19f155347878d48d35e34cf50a5fa56c
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-10-05 16:56:55 -0300

    feat: create findUserByPhone method

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -3,6 +3,7 @@ import { Prisma, User } from '@prisma/client'
 export interface UsersRepository {
   create(data: Prisma.UserCreateInput): Promise<User>
   findUserByEmail(email: string): Promise<User | null>
+  findUserByPhone(phone: string): Promise<User | null>
   findUserById(id: string): Promise<User | null>
   userUpdate(data: Prisma.UserUpdateInput): Promise<User>
 }

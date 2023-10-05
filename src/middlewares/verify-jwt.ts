a681b593eb20c9cdc334cf5f552a23c5c8d54302
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-10-05 18:52:11 -0300

    feat: create message of token errors

--- src/middlewares/verify-jwt.ts
+++ src/middlewares/verify-jwt.ts
@@ -1,5 +1,21 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
+const myCustomMessages = {
+  badRequestErrorMessage: 'Format is Authorization: Bearer [token]',
+  badCookieRequestErrorMessage: 'Cookie could not be parsed in request',
+  noAuthorizationInHeaderMessage:
+    'No Authorization was found in request.headers',
+  noAuthorizationInCookieMessage:
+    'No Authorization was found in request.cookies',
+  authorizationTokenExpiredMessage: 'Authorization token expired',
+  authorizationTokenUntrusted: 'Untrusted authorization token',
+  authorizationTokenUnsigned: 'Unsigned authorization token',
+
+  authorizationTokenInvalid: (err: any) => {
+    return `Authorization token is invalid: ${err.message}`
+  },
+}
+
 export async function verifyJWT(request: FastifyRequest, reply: FastifyReply) {
   const tokenHeader = request.headers.authorization
 
@@ -10,6 +26,8 @@ export async function verifyJWT(request: FastifyRequest, reply: FastifyReply) {
   try {
     await request.jwtVerify()
   } catch (err) {
-    return reply.status(401).send({ message: `Invalid token: ${err}` })
+    return reply.status(401).send({
+      message: `${myCustomMessages.authorizationTokenInvalid(err)}`,
+    })
   }
 }

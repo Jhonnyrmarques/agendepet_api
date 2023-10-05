5d04a83126de326f73256fc34851420d82f3656f
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-10-05 16:56:17 -0300

    feat: create existing phone verification

--- src/usecases/users/user-register.ts
+++ src/usecases/users/user-register.ts
@@ -3,6 +3,7 @@ import { Role, User } from '@prisma/client'
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
+import { PhoneAlreadyExistsError } from '@/errors/phone-already-exists-error'
 
 interface UserRegisterUseCaseRequest {
   first_name: string
@@ -29,9 +30,15 @@ export class UserRegisterUseCase {
     role,
   }: UserRegisterUseCaseRequest): Promise<UserRegisterUseCaseResponse> {
     const userAlreadyExists = await this.usersRepository.findUserByEmail(email)
+    const userPhoneAlreadyExists =
+      await this.usersRepository.findUserByPhone(phone)
 
     if (userAlreadyExists) {
       throw new UserAlreadyExistsError()
+    }
+
+    if (userPhoneAlreadyExists) {
+      throw new PhoneAlreadyExistsError()
     }
 
     const password = await hash(password_hash, 6)

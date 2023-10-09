3995c0e821f49760bea701a5817afdd32c761129
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-10-09 17:03:47 -0300

    feat: create errorTokenMessagesReturn

---
+++ src/utils/jwtmessages/index.ts
@@ -0,0 +1,15 @@
+export const errorTokenMessagesReturn = {
+  badRequestErrorMessage: 'Format is Authorization: Bearer [token]',
+  badCookieRequestErrorMessage: 'Cookie could not be parsed in request',
+  noAuthorizationInHeaderMessage:
+    'No Authorization was found in request.headers',
+  noAuthorizationInCookieMessage:
+    'No Authorization was found in request.cookies',
+  authorizationTokenExpiredMessage: 'Authorization token expired',
+  authorizationTokenUntrusted: 'Untrusted authorization token',
+  authorizationTokenUnsigned: 'Unsigned authorization token',
+
+  authorizationTokenInvalid: (err: any) => {
+    return `Authorization token is invalid: ${err.message}`
+  },
+}

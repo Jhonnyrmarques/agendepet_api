824791dea03dec7aa56be30d89c23967e825ab2d
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-11-17 18:55:05 -0300

    feat: add refresh token validations

--- src/usecases/users/refresh-token.ts
+++ src/usecases/users/refresh-token.ts
@@ -1,4 +1,5 @@
 import { env } from '@/env'
+import { ErrorMessages } from '@/errors/error-messages'
 import { RefreshTokensRepository } from '@/repositories/refresh-tokens-repository'
 import { DayJs } from '@/utils/dateConvert/dayjs'
 
@@ -11,21 +12,34 @@ export class RefreshTokenUseCase {
   constructor(private refreshTokensRepository: RefreshTokensRepository) {}
 
   async execute({ user_id, refresh_token }: RefreshTokenUseCaseRequest) {
-    const userToken =
-      await this.refreshTokensRepository.findRefreshTokenByUserId(user_id)
-
-    if (userToken) {
-      await this.refreshTokensRepository.deleteById(userToken.id)
-    }
-
-    const expires_in = new DayJs().addDays(
+    const dateExpires = new DayJs().addDays(
       Number(env.JWT_REFRESH_TOKEN_EXPIRESS.charAt(0)),
     )
 
-    await this.refreshTokensRepository.create({
-      user_id,
-      refresh_token,
-      expires_in,
-    })
+    const userRefreshToken =
+      await this.refreshTokensRepository.findRefreshTokenByUserId(user_id)
+
+    if (userRefreshToken) {
+      const expiredDate = new DayJs().compareDateIsBefore(
+        userRefreshToken.expires_in,
+      )
+
+      if (expiredDate) {
+        await this.refreshTokensRepository.deleteById(userRefreshToken.id)
+        throw new ErrorMessages('Session expired!')
+      }
+
+      await this.refreshTokensRepository.updateRefreshToken({
+        id: userRefreshToken.id,
+        refresh_token,
+        updated_at: new Date(),
+      })
+    } else {
+      await this.refreshTokensRepository.create({
+        refresh_token,
+        user_id,
+        expires_in: dateExpires,
+      })
+    }
   }
 }

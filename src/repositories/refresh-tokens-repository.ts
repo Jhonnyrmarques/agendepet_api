acd976a8c3193bbe5521958d8c1c7b26171aa21e
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-11-17 18:52:31 -0300

    feat: create updateRefreshToken method

--- src/repositories/refresh-tokens-repository.ts
+++ src/repositories/refresh-tokens-repository.ts
@@ -3,5 +3,8 @@ import { Prisma, RefreshToken } from '@prisma/client'
 export interface RefreshTokensRepository {
   create(data: Prisma.RefreshTokenUncheckedCreateInput): Promise<RefreshToken>
   findRefreshTokenByUserId(user_id: string): Promise<RefreshToken | null>
+  updateRefreshToken(
+    data: Prisma.RefreshTokenUncheckedUpdateInput,
+  ): Promise<RefreshToken>
   deleteById(id: string): Promise<void>
 }
